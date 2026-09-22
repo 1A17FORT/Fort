f3263cf7f25142869c4f0f743f3ffb99f7e8bef8
1A17FORT <b7541275412@gmail.com>
2026-09-22 12:32:11 +0300

    Second
    functions

---
+++ index2.php
@@ -0,0 +1,15 @@
+<?php
+
+// function test()
+// {
+//     echo 'Hello';
+// }
+// test();
+// test();
+function hello($x = 'Guest')
+{
+    echo "Hello, $x";
+}
+hello();
+hello('John');
+hello('jack');
