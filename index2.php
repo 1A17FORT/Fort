a2b1553124a192637333ba1bfde77ea61480b60e
1A17FORT <b7541275412@gmail.com>
2026-09-22 13:07:29 +0300

    Update index2.php

--- index2.php
+++ index2.php
@@ -25,3 +25,37 @@
 // var_dump($a);
 // test($a);
 // var_dump($a);
+
+
+// function sum(... $nums)
+// {
+//     $res = 0;
+//     foreach($nums as $num)
+//         {
+//             $res += $num;
+//         }
+//     echo $res;
+// }
+// sum(10, 10, 20, 1,);
+
+// function sum($a, $b, $c):int
+// {
+//     return $a + $b + $c;
+// }
+// var_dump(sum(1,2,4));
+
+function table(int $trfirst, int $tdfirst)
+{
+    echo '<table border="1" width="100%">';
+    for ($tr = 1; $tr < $trfirst; $tr++)
+        {
+            echo "<tr>";
+            for ($td = 1; $td < $tdfirst; $td++)
+                {
+                    echo "<td> $tr * $td = " . $td*$tr . "</td>";
+                }
+                echo "</tr>";
+        }
+    echo '</table>';
+}
+table(2,5);
