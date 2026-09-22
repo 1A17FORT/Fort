a1ab24cea988fd2314670502ce8a4391818f4f8a
1A17FORT <b7541275412@gmail.com>
2026-09-22 12:42:09 +0300

    Update index2.php

--- index2.php
+++ index2.php
@@ -6,10 +6,22 @@
 // }
 // test();
 // test();
-function hello($x = 'Guest')
-{
-    echo "Hello, $x";
-}
-hello();
-hello('John');
-hello('jack');
+
+// function hello($x = 'Guest')
+// {
+//     echo "Hello, $x";
+// }
+// hello();
+// hello('John');
+// hello('jack');
+
+// $a = 5;
+// function test($a)
+// {
+//     global $a; Для использования вне функции
+//     $a+=10;
+//     var_dump($a);
+// }
+// var_dump($a);
+// test($a);
+// var_dump($a);
